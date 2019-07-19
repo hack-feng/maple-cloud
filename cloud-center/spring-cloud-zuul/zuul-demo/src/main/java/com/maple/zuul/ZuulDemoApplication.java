4e7b21512e81a5231b79efb7b1bcdbff22d3f618
hack-feng <1150640979@qq.com>
2019-07-19 18:06:33 +0800

    springboot升级2.0，添加admin图形界面监控

--- cloud-center/spring-cloud-zuul/zuul-demo/src/main/java/com/maple/zuul/ZuulDemoApplication.java
+++ cloud-center/spring-cloud-zuul/zuul-demo/src/main/java/com/maple/zuul/ZuulDemoApplication.java
@@ -3,11 +3,11 @@ package com.maple.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulServer
+@EnableZuulProxy
 public class ZuulDemoApplication {
 
     public static void main(String[] args) {
--- project-center/maple-service/user-service/src/main/java/com/maple/user/controller/BaseUserController.java
+++ project-center/maple-service/user-service/src/main/java/com/maple/user/controller/BaseUserController.java
@@ -1,8 +1,10 @@
 package com.maple.user.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.maple.user.service.IBaseUserService;
 import com.maple.userapi.bean.BaseUser;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +29,43 @@ public class BaseUserController {
     @Autowired
     private IBaseUserService userService;
 
+    @HystrixCommand(fallbackMethod = "baseHys")
     @GetMapping(value = "getList")
-    public List<BaseUser> getList(){
+    public JSONObject getList(){
+        JSONObject result = new JSONObject();
         List<BaseUser> list = userService.list(null);
-        return list;
+        result.put("code", 200);
+        result.put("data", list);
+        return result;
+    }
+
+    private JSONObject baseHys(){
+        JSONObject a = new JSONObject();
+        a.put("code", 500);
+        a.put("data", "BaseUserController的线路中断");
+        return a;
+    }
+
+    @HystrixCommand(fallbackMethod = "getUserInfoHys")
+    @RequestMapping(value = "getUserInfo")
+    public JSONObject getUserInfo(Integer id){
+        JSONObject result = new JSONObject();
+        BaseUser user = null;
+        try {
+            user = userService.getById(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        result.put("code", 200);
+        result.put("data", user);
+        return result;
+    }
+
+    private JSONObject getUserInfoHys(Integer id){
+        JSONObject a = new JSONObject();
+        a.put("code", 500);
+        a.put("data", "BaseUserController的线路中断");
+        return a;
     }
 
 }
